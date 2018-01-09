2fefbed98b38f4cd89201eebbb7ace9a460955de
Reckziegel <bernardo_cse@hotmail.com>
2018-01-09 14:56:35 -0200

    kernel_PL function. sanity check: OK!

--- kernel_PL.R
+++ kernel_PL.R
@@ -14,21 +14,30 @@
 # v1 = normpdf((Z - X)/h);
 # f = mean(v1)/h;
 
-kernel_PL <- function(Z, X, bandw) {
+kernel_PL <- function(Z, X, bandw = 0) {
     
     #Kernel density estimator
-    nobs <- nrow(X)
-    #n <- nrow(Z)
-    Q1 <- quantile(X, 0.25)
-    Q3 <- quantile(X, 0.75)
+    if (class(X) == 'numeric') {
+        nobs <- length(X)
+    } else if (class(X) == 'matrix') {
+        nobs <- nrow(X)
+    } else {
+            stop("you should continue working with this function!")
+    }
     
-    if (nargs(kernel_PL) == 2) {
-        h <- 0.79 * (Q3 - Q1) * nobs ^ (-0.2)
+    #n <- nrow(Z)
+    Q1 <- round(quantile(X, 0.25), 0)
+    Q3 <- round(quantile(X, 0.75), 0)
+
+    if (bandw == 0) {
+        h <- 0.79 * (Q3 - Q1) * round(nobs ^ (-0.2), 0)
     } else {
         h <- bandw
     }
     
     v1 <- dnorm((Z - X) / h)
     f <- mean(v1) / h
-    
+    #return(f)
 }
+
+
