6bab1ae25d27e6ec2baea9357bb24bc359ab6acb
Reckziegel <bernardo_cse@hotmail.com>
2018-01-09 01:02:36 -0200

    standardize1 function.
    (standardize data) using mean and variance from training sample.)

---
+++ standardize1.R
@@ -0,0 +1,13 @@
+# function y = standardize1(x,T_thres)
+# 
+# % standardize using mean and variance from training sample
+# % Data in x are Txp, i.e. T time series observations times p variables
+# y = (x - repmat(mean(x(1:T_thres,:),1),size(x,1),1))./repmat(std(x(1:T_thres,:),1),size(x,1),1);
+
+standardize1 <- function(x, T_thres) {
+    # standardize using mean and variance from training sample
+    # Data in x are Txp, i.e. T time series observations times p variables
+    
+    y <- (x - apply(x[1:T_thres, ], 2, mean)) / apply(x[1:T_thres, ], 2, sd)
+
+}
