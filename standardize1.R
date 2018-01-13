8e6e52cb7c43fd09dea2211f9d323ff105c94e99
Reckziegel <bernardo_cse@hotmail.com>
2018-01-13 18:23:42 -0200

    just a review.

--- standardize1.R
+++ standardize1.R
@@ -8,6 +8,13 @@ standardize1 <- function(x, T_thres) {
     # standardize using mean and variance from training sample
     # Data in x are Txp, i.e. T time series observations times p variables
     
-    y <- (x - apply(x[1:T_thres, ], 2, mean)) / apply(x[1:T_thres, ], 2, sd)
+    y <- (x - repmat(t(apply(x[1:T_thres, ], 2, mean)), nrow(x), 1)) / repmat(t(apply(x[1:T_thres, ], 2, sd)), nrow(x), 1)
+    y
+    
+    #scale(x[1:T_thres, ], center = TRUE)
+    
+    # Still have problems with this one!
+    # Answers are different then those provided by matlab.
 
 }
+
