4718f73ed721d03d23a97860bd84b303ebc2dc80
Reckziegel <bernardo_cse@hotmail.com>
2018-01-14 20:02:31 -0200

    Inefficient but similar to koop's code.

--- standardize.R
+++ standardize.R
@@ -9,7 +9,8 @@ standardize <- function(x) {
     
     # Function to make your data have mean 0 and variance 1.
     # Data in x are od dimmension Txp, i.e. T time series observations times p variables
+    y <- (x - repmat(t(apply(x, 2, mean)), nrow(x), 1)) / repmat(t(apply(x, 2, sd)), nrow(x), 1)
+    y
     
-    scale(x, center = TRUE)
-    
+    #scale(x, center = TRUE)
 }
