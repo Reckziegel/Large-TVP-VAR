6cb84af237032a40a9b154ee7fc92dd553eca0ca
Reckziegel <bernardo_cse@hotmail.com>
2018-01-14 22:48:44 -0200

    some corrections on matrix dimensions.

--- mvfactor.R
+++ mvfactor.R
@@ -1,14 +1,18 @@
 mvnfactor <- function(sigma) {     
     # originally [T,p] = 
-    # MVNFACTOR  Do Cholesky-like decomposition, allowing zero eigenvalues
+    # MVNFACTOR  Do Cholesky-like decomposition, allowing sigmaero eigenvalues
     # SIGMA must be symmetric.  In general T is not square or triangular.
     
-    U <- eigen(sigma + t(sigma))$vectors / 2
-    D <- eigen(sigma + t(sigma))$values
+    eps = 0.01
+    
+    U <- eigen(sigma + t(sigma))$vectors
+    U <- U[ , order(ncol(U):1)] 
+    D <- eigen(sigma + t(sigma))$values / 2
+    D <- rev(D)
 
     tol <- max(D) * length(D) * eps # check where "eps" comes form. I do not remember.
     t <- D > tol
-    D <- D(t)
-    T <- diag(sqrt(D)) %*% t(U[ , t])
+    D <- D[t]
+    T <- -sqrt(D) * t(U[ , t])
     return(T)
 }
