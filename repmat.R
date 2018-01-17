6904dbfff23a78e15a9f35bcf64fb2f899149201
Reckziegel <bernardo_cse@hotmail.com>
2018-01-17 01:20:51 -0200

    The same as the repmat function in MATLAB.

--- repmat.R
+++ repmat.R
@@ -1,9 +1,5 @@
 repmat <- function(x, m, n) {
     
-    if (class(data) != 'matrix') {
-        x <- as.matrix(x)
-    }
-    
     mx <- dim(x)[1]
     nx <- dim(x)[2]
     matrix(data  = t(matrix(data = x, nrow = mx, ncol = nx * n)), 
