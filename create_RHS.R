416416d3c1c85104160c3c07838980d276ea7344
Reckziegel <bernardo_cse@hotmail.com>
2018-01-17 01:11:19 -0200

    I took off the matlab part.

--- create_RHS.R
+++ create_RHS.R
@@ -1,20 +1,3 @@
-# function [x_t,K] = create_RHS(YY,M,p,t)
-# 
-# K = M + p*(M^2); % K is the number of elements in the state vector
-# 
-# % Create x_t matrix.
-# % first find the zeros in matrix x_t
-# x_t = zeros((t-p)*M,K,'single');
-# for i = 1:t-p
-# ztemp = eye(M);
-# for j = 1:p
-# xtemp = YY(i,(j-1)*M+1:j*M);
-# xtemp = kron(eye(M),xtemp);
-# ztemp = [ztemp xtemp];
-# end
-# x_t((i-1)*M+1:i*M,:) = ztemp;
-# end
-
 create_RHS <- function(YY, M, p, t) {
     
     K <- M + p * (M ^ 2) # K is the number of elements in the state vector
